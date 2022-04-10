f8a29b9cec6b1222701a5d110fa78e86be4fa2ed
Marcos Sousa <a.marcos.car@bol.com.br>
2022-04-10 19:18:38 -0300

    JS: primeira parte de coonsumo de API concluido

--- app_animal2/public/index.js
+++ app_animal2/public/index.js
@@ -1,1 +1,49 @@
-alert ("Java TDS")
+// Puxar requisições do banckend 
+async function carregarAnimais(){
+    const response  = await axios.get('http://localhost:8000/animais')
+        
+    const animais = response.data
+
+    const lista = document.getElementById('lista-animais')
+
+    lista.innerHTML = ''
+    animais.forEach(animal => {
+        const item = document.createElement('li')
+
+        const linha = `${animal.nome} - idade: ${animal.idade} - cor: ${animal.cor}`
+        item.innerText = linha
+
+        lista.appendChild(item)
+    });
+}
+// Mandar requisição para o backend 
+
+function manipularFormulario(){
+    const form_animal = document.getElementById('form-animal')
+    const input_nome = document.getElementById('nome')
+
+    form_animal.onsubmit = async (event) => {
+        event.preventDefault()
+        const nome_animal = input_nome.value
+
+        await axios.post('http://localhost:8000/animais', {
+            nome: nome_animal,
+            idade: 4,
+            sexo: 'femea',
+            cor: 'branco'   
+        })
+        carregarAnimais()
+        alert('Animal cadastrado')
+
+
+    }
+}
+ 
+
+function app(){
+    console.log('App iniciada')
+    carregarAnimais()
+    manipularFormulario()
+}
+
+app()
--- app_animal2/src/server.py
+++ app_animal2/src/server.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 from typing import List, Optional
 from pydantic import BaseModel
 from uuid import uuid4
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ['http://localhost:5500']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 class Animal(BaseModel):
     id: Optional[int]
