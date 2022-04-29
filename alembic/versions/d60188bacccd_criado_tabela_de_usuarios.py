965279cf616d5168a432ee0e535b77a79a3ce112
Marcos Sousa <a.marcos.car@bol.com.br>
2022-04-29 00:00:01 -0300

    Repositorio Usuarios

---
+++ alembic/versions/d60188bacccd_criado_tabela_de_usuarios.py
@@ -0,0 +1,24 @@
+"""criado tabela de usuarios
+
+Revision ID: d60188bacccd
+Revises: 
+Create Date: 2022-04-28 22:39:51.452840
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd60188bacccd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
